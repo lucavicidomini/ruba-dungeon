3aa99b1970cb0fe87647b092aef032e81f750c2e
Luca Vicidomini <vicidomini@gmail.com>
2024-05-23 10:02:06 +0200

    Deck template, cover cards

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Card } from '../+models/card.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
 
   @Input() card!: Card;
+
+  @Input() cover = false;
 
   get suit() {
     switch (this.card.suit) {
--- src/app/deck/deck.component.ts
+++ src/app/deck/deck.component.ts
@@ -1,15 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Deck } from '../+models/deck.model';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-deck',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './deck.component.html',
   styleUrl: './deck.component.scss'
 })
 export class DeckComponent {
 
   @Input() deck = Deck.empty();
+
+  @Input() cover = false;
 
 }
