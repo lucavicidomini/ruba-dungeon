8eb81117433fc5f3b5c55c4300283963402777de
Luca Vicidomini <vicidomini@gmail.com>
2024-05-24 06:54:16 +0200

    Remove EnableIfStatusDirective

--- src/app/directives/enable-if-status.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EnableIfStatusDirective } from './enable-if-status.directive';
-
-describe('StatusIsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new EnableIfStatusDirective();
-    expect(directive).toBeTruthy();
-  });
-});
