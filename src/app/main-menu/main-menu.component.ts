00f8e35798374413a151a22fd4cf318826df9c8b
Luca Vicidomini <vicidomini@gmail.com>
2024-05-29 11:20:38 +0200

    Main menu UI

---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.scss'
+})
+export class MainMenuComponent {
+
+  onNewGame() {
+
+  }
+
+  onSettings() {
+
+  }
+
+  onAbout() {
+
+  }
+
+}
